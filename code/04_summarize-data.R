11fdc1cfaa9fc92be98094058975a41c32bd9474
kristinjbarker <kristinjbarker@gmail.com>
2026-01-21 11:59:57 -0700

    use geopackage instead of indiv shps. start data summaries

--- code/04_summarize-data.R
+++ code/04_summarize-data.R
@@ -1,17 +1,41 @@
-source("00_config.R")
+source("code/00_config.R")
 
 # from 03_clean-fencelines.R
-spatFences <- st_read("data/processed/fencelines.shp")
-spatFeatures <- st_read("data/processed/fenceFeatures.shp")
-spatRange <- st_read("data/processed/rangeImprovements.shp")
-spatWildlife <- st_read("data/processed/wildlifeSign.shp")
 
-# from project planning
+# processed spatial data
+spatFences <- st_read("data/processed/fenceInventory.gpkg", layer = "fencelines")
+spatFeatures  <- st_read("data/processed/fenceInventory.gpkg", layer = "fenceFeatures")
+spatRange <- st_read("data/processed/fenceInventory.gpkg", layer = "rangeImprovements")
+spatWildlife   <- st_read("data/processed/fenceInventory.gpkg", layer = "wildlifeSign")
+
+# spatial data from project planning
 priority_areas <- read_sf("data/project-specific/Priority_Inventory_areas.shp")
 pronghorn_range <- read_sf("data/project-specific/projectarea2025.shp")
 
 
-## FILTER TO CODY REGION
+# fenceline summary table
+sumFences <- spatFences %>%
+  # for fences that have been mapped,
+  filter(Status == "Inventoried") %>%
+  # calculate length of each fence segment
+  mutate(length_m = as.numeric(st_length(geom))) %>%
+  # sum length of fence per thing of interest
+  st_drop_geometry() %>%
+  group_by(
+    Year,
+    Group,
+    Condition,
+    Problem) %>%
+  summarize(Meters = sum(length_m, na.rm = TRUE),
+    .groups = "drop") %>%
+  # convert m to mi
+  mutate(Miles = 0.0006213712 * Meters)
+
+sumYr <- sumFences %>%
+  group_by(Year) %>%
+  summarize(Miles = sum(Miles, na.rm = TRUE))
+
+
 
 
 # DO NOT SOURCE OTHER CODE just read in their outputs. can use ifelse to check.
@@ -21,19 +45,9 @@ summary_overall <- anotherFormattedTibble
 
 ## you'll pull numbers from these, eg total_miles <- summary_overall$total_miles
 
-# summarize (a) for 2025 and (b) overall
-# number of weeks?
-# miles fence mapped
-#   miles mapped by BYLL/mapped by others
-#   miles fence in need of assessment for removal/repair
-#   miles poor condition fences
-# separate est for fences in poor condition or urgent within highest use areas?
 # number braces
 # number gates
 # number mortalities
 # number fence crossings
-# 
-# number individuals who collected data
-# number groups who helped collect data
-# 2025 only - area of pronghorn range? area of priority areas? only BLM area within?
+
 
